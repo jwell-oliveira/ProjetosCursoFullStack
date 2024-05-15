f91285b2506edd0d90f321d069a0f51311ddf460
Wellington Oliveira <jwell.oliveira@gmail.com>
2024-05-15 20:02:22 -0300

    Endpoint Read ALL /pensonagens implementado

--- Missão2/index.js
+++ Missão2/index.js
@@ -4,5 +4,11 @@ const app = express()
 app.get('/', function (req, res) {
   res.send('Hello World!')
 })
+const list = ['java','kotlin','android']
 
+// EndPoint Read ALL [GET] /pensonagens
+
+app.get('/pensonagens', function (req, res){
+  res.send(list)
+})
 app.listen(3000)
