7944201ad305a610602489b4174c1874de92ca17
Wellington Oliveira <jwell.oliveira@gmail.com>
2024-05-18 10:23:43 -0300

    Implementação Endpoint delete

--- Missão2/index.js
+++ Missão2/index.js
@@ -1,31 +1,76 @@
-const express = require('express')
-const app = express()
+// Endpoint Create [POST] /personagem
+app.post('/personagem', function (req, res) {
+  // Acessamos o Body da Requisição
+  const body = req.body
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
+  // Acessamos a propriedade `nome` do body
+  const novoItem = body.nome
+
+  // Checar se o `nome` está presente no body
+  if (!novoItem) {
+    return res.status(400).send('Corpo da requisição deve conter a propriedade `nome`.')
+  }
+
+  // Checar se o novoItem está na lista ou não
+  if (lista.includes(novoItem)) {
+    return res.status(409).send('Esse item já existe na lista.')
+  }
+
+  // Adicionamos na lista
+  lista.push(novoItem)
+
+  // Exibimos uma mensagem de sucesso
+  res.status(201).send('Item adicionado com sucesso: ' + novoItem)
 })
-const list = ['java','kotlin','android']
 
-// EndPoint Read ALL [GET] /pensonagens
+// Endpoint Update [PUT] /personagem/:id
+app.put('/personagem/:id', function (req, res) {
+  // Acessamos o ID dos parâmetros de rota
+  const id = req.params.id
 
-app.get('/pensonagens', function (req, res){
-  res.send(list)
+  // Checamos se o item do ID - 1 está na lista, exibindo
+  // uma mensagem caso não esteja
+  if (!lista[id - 1]) {
+    return res.status(404).send('Item não encontrado.')
+  }
+
+  // Acessamos o Body da requisição
+  const body = req.body
+
+  // Acessamos a propriedade `nome` do body
+  const novoItem = body.nome
+
+  // Checar se o `nome` está presente no body
+  if (!novoItem) {
+    return res.status(400).send('Corpo da requisição deve conter a propriedade `nome`.')
+  }
+
+  // Checar se o novoItem está na lista ou não
+  if (lista.includes(novoItem)) {
+    return res.status(409).send('Esse item já existe na lista.')
+  }
+
+  // Atualizamos na lista o novoItem pelo ID - 1
+  lista[id - 1] = novoItem
+
+  // Enviamos uma mensagem de sucesso
+  res.send('Item atualizado com sucesso: ' + id + ' - ' + novoItem)
 })
 
-// EndPoint Read by id [GET] /pensonagens:id
-app.get('/pensonagens/:id', function (req, res){
-    const id = req.params.id
+// Endpoint Delete [DELETE] /personagem/:id
+app.delete('/personagem/:id', function (req, res) {
+  // Acessamos o parâmetro de rota
+  const id = req.params.id
 
-    const item = list[id - 1]
-    
-    res.send(item)
+  // Checamos se o item do ID - 1 está na lista, exibindo
+  // uma mensagem caso não esteja
+  if (!lista[id - 1]) {
+    return res.status(404).send('Item não encontrado.')
+  }
+
+  // Remover o item da lista usando o ID - 1
+  delete lista[id - 1]
+
+  // Enviamos uma mensagem de sucesso
+  res.send('Item removido com sucesso: ' + id)
 })
-
-// EndPoint Create [POST] /pensonagens
-app.post('/pensonagens', function (req, res){
-  const novoitem = 'HTML'
-  list.push(novoitem)
-  res.send(list)
-
-})
-app.listen(3000)
